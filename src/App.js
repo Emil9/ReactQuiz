90301a808c8c245632cd693e4510a15b71c150c7
Emil9 <emil.grabowski@interia.pl>
2020-05-15 22:16:17 +0200

    Last commit

--- src/App.js
+++ src/App.js
@@ -1,39 +1,82 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import ListaFiszek from './ListaFiszek';
 import './App.css';
+import axios from 'axios';
 
-const API = 'https://opentdb.com/api.php?amount=10'
 
 function App() {
-  const [fiszki, setFiszki] = useState(FISZKA_PRZYKLAD)
+  const [fiszki, setFiszki] = useState([])
+  const [kategorie, setKategorie] = useState([])
+
+  const kategoriaEl = useRef()
+  const iloscEl = useRef()
+
+  useEffect(() => {
+    axios.get('https://opentdb.com/api_category.php')
+    .then(res => {
+      setKategorie(res.data.trivia_categories)
+    })
+  }, [])
+
+  useEffect(() => {
+    
+  }, [])
+
+  function dekodujTekst(str) {
+    const textArea = document.createElement('textarea')
+    textArea.innerHTML = str
+    return textArea.value
+  }
+
+  function submit(e) {
+    e.preventDefault()
+    axios.get('https://opentdb.com/api.php', {
+      params: {
+        amount: iloscEl.current.value,
+        category: kategoriaEl.current.value
+      }
+    })
+      .then(res => {
+        setFiszki(res.data.results.map((pytanieItem, index) => {
+          const answer = dekodujTekst(pytanieItem.correct_answer)
+          const options = [
+            ...pytanieItem.incorrect_answers.map(a => dekodujTekst(a)),
+            answer]
+          return {
+            id: `${index}-${Date.now()}`,
+            question: dekodujTekst(pytanieItem.question),
+            answer: answer,
+            options: options.sort(() => Math.random() - .5)
+          }
+        }))
+      })
+  }
+
   return (
-    <ListaFiszek fiszki={fiszki} />
+    <>
+    <form className="header" onSubmit={submit}>
+      <div className="form-grupa">
+        <label htmlFor="category">Kategoria</label>
+        <select id="category" ref={kategoriaEl} >
+          {kategorie.map(kategoria => {
+            return <option value={kategoria.id} key={kategoria.id}>{kategoria.name}</option>
+          })}
+          
+        </select>
+      </div>
+        <div className="form-grupa">
+          <label htmlFor="ilosc">Ilość pytań</label>
+          <input type="numer" id="ilosc" min="1" step="1" defaultValue={10} ref= {iloscEl} />
+        </div>
+        <div className="form-grupa">
+          <button className="btn">Generuj</button>
+        </div>
+    </form>
+      <div className="kontener">
+       <ListaFiszek fiszki={fiszki} />
+      </div>
+    </>
   );
 }
-
-const FISZKA_PRZYKLAD = [
-  {
-    id: 1,
-    pytanie: 'Ile to 2 + 2?',
-    odpowiedz: '4',
-    mozliwosci: [
-      '2',
-      '3',
-      '4',
-      '5'
-    ]
-  },
-  {
-    id: 2,
-    pytanie: 'Pytanie?',
-    odpowiedz: 'Odpowiedź',
-    mozliwosci: [
-      'Odpowiedź',
-      'Odpowiedź 1',
-      'Odpowiedź 2',
-      'Odpowiedź 3'
-    ]
-  }
-]
 
 export default App;
--- src/Fiszka.js
+++ src/Fiszka.js
@@ -1,23 +1,39 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 
 export default function Fiszka({ fiszka }) {
     const [odwrot, setOdwrot] = useState(false)
+    const [height, setHeight] = useState('initial')
 
+    const przodEl = useRef()
+    const tylEl = useRef()
+
+    function setMaxHeight() {
+        const przodHeight = przodEl.current.getBoundingClientRect().height
+        const tylHeight = tylEl.current.getBoundingClientRect().height
+        setHeight(Math.max(przodHeight, tylHeight, 100))
+    }
+
+    useEffect(setMaxHeight, [fiszka.question, fiszka.answer, fiszka.options])
+    useEffect(() => {
+        window.addEventListener('resize', setMaxHeight)
+        return () => window.removeEventListener('resize', setMaxHeight)
+    }, [])
     return (
         <div 
         className={`karta ${odwrot ? 'odwrot' : ''}`}
+        style={{ height: height}}
         onClick={() => setOdwrot(!odwrot)}
         >
-            <div className="przod">
-                {fiszka.pytanie}
-                <div className="fiszka-mozliwosci">
-                    {fiszka.mozliwosci.map(mozliwosc => {
-                        return <div className="fiszka-mozliwosc">{mozliwosc}</div>
+            <div className="przod" ref={przodEl}>
+                {fiszka.question}
+                <div className="fiszka-options">
+                    {fiszka.options.map(option => {
+                        return <div className="fiszka-option" key={option}>{option}</div>
                     })}
                 </div>
             </div>
-            <div className="tyl">
-                {fiszka.odpowiedz}
+            <div className="tyl" ref={tylEl}>
+                {fiszka.answer}
             </div>
         </div>
     )
